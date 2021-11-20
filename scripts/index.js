662fb09a3ee905075b41626e42d6dab3829a42f8
Igor Trubkin <b-rey@inbox.ru>
2021-11-20 17:35:53 +0300

    added hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,33 @@
 
-const editButton = document.querySelector('.profile__edit-button_type_open');
+const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseIkon = document.querySelector('.popup__close-ikon');
-
-function open() {
-   popup.classList.add('popup_type_open');
-   nameInput.value = profileName.textContent;
-   workInput.value = profileWork.textContent;
-}
-
-function cloce() {
-   popup.classList.remove('popup_type_open');
-}
-
-editButton.addEventListener('click', open);
-popupCloseIkon.addEventListener('click', cloce);
-
-
-
 const form = document.querySelector('.popup__container');
 const nameInput = document.querySelector('.popup__input_type_name');
 const workInput = document.querySelector('.popup__input_type_work');
 const profileName = document.querySelector('.profile__name');
 const profileWork = document.querySelector('.profile__work');
 
+
+function openPopup() {
+   popup.classList.add('popup_open');
+   nameInput.value = profileName.textContent;
+   workInput.value = profileWork.textContent;
+}
+
+
+function closePopup() {
+   popup.classList.remove('popup_open');
+}
+
+
 function formSubmitHandler (evt) {
    evt.preventDefault();
    profileName.textContent = nameInput.value;
    profileWork.textContent = workInput.value;
-   cloce()
+   closePopup()
 }
 
 form.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+popupCloseIkon.addEventListener('click', closePopup);
